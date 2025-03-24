15c834cfbeb031f88e6aa6af5060469c32b8a684
Hamza Benmohamd <hbenmoha@e2r2p15.1337.ma>
2025-03-24 08:32:53 +0000

    fix bug in str_capitalizer

--- level_3/str_capitalizer/str_capitalizer.c
+++ level_3/str_capitalizer/str_capitalizer.c
@@ -20,28 +20,26 @@ void to_lower(char *str)
 
 void to_uppercase(char *str)
 {
-	while (*str)
+	int i = 0;
+
+	while (str[i])
 	{
-		if (*str == 32 && ((*(str + 1) >= 97) && (*(str + 1) <= 122) && *str))
-			*(str + 1) -= 32;
-		str++;
+		//* If it's the first character or follows a space/tab
+		if ((i == 0 || str[i - 1] == ' ' || str[i - 1] == '\t') && (str[i] >= 'a' && str[i] <= 'z'))
+			str[i] -= 32;
+		i++;
 	}
-	
 }
 
 int main (int ac, char *av[])
 {
-	int i = 1, j = 0;
+	int i = 1;
 	if (ac == 1)
 		return (write(1 , "\n", 1));
 
 	while (i < ac)
 	{
 		to_lower(av[i]);
-		if (av[i][j] == 32 || av[i][j] == 9)
-			j++;
-		if (av[i][j] && (av[i][j] >= 97 && av[i][j] <= 122))
-			av[i][j] -= 32;
 		to_uppercase(av[i]);
 		ft_putstr(av[i]);
 		write(1, "\n", 1);
